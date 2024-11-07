72c2d4b193f74d9ad04b37a423dd90d084cedf9b
NeoMetalx1 <153954385+NeoMetalx1@users.noreply.github.com>
2024-11-07 19:34:36 +0200

    Add files via upload

---
+++ CLI_messenger.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+	
+	string username;
+	string user_input;
+
+	cout << "Enter username: ";
+	cin >> username;
+	cout << endl;
+	
+	ofstream Chat;
+	Chat.open("chat.txt", ios::app);
+
+	while (true) {
+		cout << "Enter text (type 'exit' to quit): ";
+		cin >> user_input;
+
+		if (user_input == "exit") {
+			break;
+		}
+
+		Chat << endl << "[" << username << "]" << " => " << user_input << endl;
+	}
+		
+	Chat.close();
+
+	return 0;
+
+}
